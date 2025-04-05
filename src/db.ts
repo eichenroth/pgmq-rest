5ff5272b194eaba883996829c969d866d96dd389
Friedrich Eichenroth <friedrich.eichenroth@gmail.com>
2025-04-05 11:23:25 +0200

    replace bun sql

--- src/db.ts
+++ src/db.ts
@@ -1,14 +1,18 @@
+import { Pool, PoolClient } from "pg";
+
 const getEnv = (name: string, defaultValue?: string): string => {
   const value = Bun.env[name] ?? defaultValue;
   if (!value) throw new Error(`ENV variable ${name} is not set!`);
   return value;
 };
 
-export const sql = new Bun.SQL({
-  hostname: getEnv("DB_HOST"),
+const db = new Pool({
+  host: getEnv("DB_HOST"),
   port: parseInt(getEnv("DB_PORT", "5432"), 10),
   database: getEnv("DB_NAME", "postgres"),
-  username: getEnv("DB_USER", "postgres"),
+  user: getEnv("DB_USER", "postgres"),
   password: getEnv("DB_PASSWORD", "postgres"),
   max: 20,
 });
+
+export const getClient = async (): Promise<PoolClient> => await db.connect();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import swagger from "@elysiajs/swagger";
 import { Elysia, t } from "elysia";
 
-import { sql } from "./db";
+import { getClient } from "./db";
 
 const app = new Elysia()
   .use(
@@ -12,11 +12,59 @@ const app = new Elysia()
   )
   .get("/", () => "Server is running")
 
+  // --- SENDING MESSAGES ---
+
+  // pgmq.send(queue_name text, msg jsonb, delay integer DEFAULT 0)
+  // RETURNS SETOF bigint
+
+  .post(
+    "/api/v1/send",
+    async ({ body: { queue_name, msg, delay = 0 } }) => {
+      const client = await getClient();
+      const result = await client.query<[string]>(
+        { rowMode: "array", text: "SELECT pgmq.send($1::text, $2::jsonb, $3::integer)", name: "send" },
+        [queue_name, msg, delay],
+      );
+      return result.rows.map(([id]) => Number(id));
+    },
+    {
+      body: t.Object({ queue_name: t.String(), msg: t.Any(), delay: t.Optional(t.Integer()) }),
+      response: t.Array(t.Number()),
+    },
+  )
+
+  // pgmq.send_batch(
+  //   queue_name text,
+  //   msgs jsonb[],
+  //   delay integer DEFAULT 0
+  // )
+  // RETURNS SETOF bigint
+
+  .post(
+    "/api/v1/send_batch",
+    async ({ body: { queue_name, msgs, delay = 0 } }) => {
+      const client = await getClient();
+      const result = await client.query<[string]>(
+        { rowMode: "array", text: "SELECT pgmq.send_batch($1::text, $2::jsonb[], $3::integer)", name: "send_batch" },
+        [queue_name, msgs, delay],
+      );
+      return result.rows.map(([id]) => Number(id));
+    },
+    {
+      body: t.Object({ queue_name: t.String(), msgs: t.Array(t.Any()), delay: t.Optional(t.Integer()) }),
+      response: t.Array(t.Number()),
+    },
+  )
+
   // --- QUEUE MANAGEMENT ---
+
+  // pgmq.create(queue_name text)
+  // RETURNS VOID
   .post(
     "/api/v1/create",
     async ({ body: { queue_name } }) => {
-      await sql`SELECT pgmq.create(queue_name => ${queue_name}::text)`;
+      const client = await getClient();
+      await client.query({ rowMode: "array", text: "SELECT pgmq.create($1::text)", name: "create" }, [queue_name]);
     },
     { body: t.Object({ queue_name: t.String() }) },
   )
